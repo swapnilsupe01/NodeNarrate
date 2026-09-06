83edc2856a8001415a4ece5cadca49a4ab23ada7
swapnilsupe01 <swapnilsupe01@gmail.com>
2026-09-06 13:55:17 +0530

    allow origin change

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app = FastAPI(title="NodeNarrate API", version="0.1.0")
 # Allow the React dev server (and later, your deployed frontend) to call this API.
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
